be98ec332ddf390b0afa32793c8aefb83e136dec
Nick Reid <nickreid@nickreid.com>
2018-08-26 15:28:40 -0700

    Style app with angular-material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MedicationSelectorComponent } from './medication-selector.component';
 import { StoryListComponent } from './story-list.component';
 
-const appRoutes:Routes = [
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+const appRoutes: Routes = [
   { path: 'medications', component: MedicationSelectorComponent },
   { path: 'stories', component: StoryListComponent },
   {
@@ -24,6 +31,12 @@ const appRoutes:Routes = [
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatRadioModule,
+    MatButtonModule,
+    MatCardModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true }
--- client/src/app/medication-selector.component.ts
+++ client/src/app/medication-selector.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
-const formChoices:any = {
+const attributes: any = {
   initiation: {
     startRightAway: {
       MMT: 2,
@@ -18,6 +19,45 @@ const formChoices:any = {
     detoxThenINJ: {
       NTXXR: 1
     }
+  },
+  sideEffects: {
+    weightGain: {
+      MMT: 2
+    },
+    drowsy: {
+      MMT: 2
+    },
+    constipation: {
+      MMT: 2
+    },
+    headaches: {
+      BPNSL: 2,
+      BPNINJ: 2,
+      BPNIMP: 2
+    },
+    insomnia: {
+      BPNSL: 2,
+      BPNINJ: 2,
+      BPNIMP: 2
+    },
+    irritationAtSite: {
+      BPNSL: 2,
+      BPNINJ: 2,
+      BPNIMP: 2
+    }
+  },
+  withdrawlSymptoms: {
+    lessSevere: {
+      MMT: 2
+    },
+    milder: {
+      BPNSL: 2
+    },
+    headaches: {
+      BPNINJ: 2,
+      BPNIMP: 2,
+      NTXXR: 2
+    }
   }
 };
 
@@ -27,4 +67,57 @@ const formChoices:any = {
 })
 export class MedicationSelectorComponent {
   title = 'client';
+  medications: Array<string>;
+
+  featuresForm = this.fb.group({
+    initiation: ['required'],
+    sideEffects: [''],
+    withdrawlSymptoms: ['']
+  });
+
+  constructor(
+    private fb: FormBuilder
+  ) {}
+
+  rankMedications(preferences: any) {
+    const medicationScores: any = {};
+
+    Object.keys(attributes).forEach((key: string) => {
+      const attributePreference: any = preferences[key];
+      if (attributePreference) {
+        const score: any = attributes[key][attributePreference];
+        if (score) {
+          Object.keys(score).forEach((medication) => {
+            if (!medicationScores[medication]) {
+              medicationScores[medication] = 0;
+            }
+            medicationScores[medication] += score[medication];
+          });
+        }
+      }
+    });
+
+    const medications: Array<string> = [];
+    Object.keys(medicationScores).forEach((medication: string) => {
+      medications.push(medication);
+    });
+    medications.sort((medA, medB) => {
+      if (medicationScores[medA] >= medicationScores[medB]) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    return medications;
+  }
+
+  updatePreferences() {
+    if (this.featuresForm.valid) {
+      this.medications = this.rankMedications({
+        initiation: this.featuresForm.get('initiation').value,
+        withdrawlSymptoms: this.featuresForm.get('withdrawlSymptoms').value
+      });
+    }
+  }
 }
