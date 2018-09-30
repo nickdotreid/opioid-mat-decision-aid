294dff2bae7ff559b93235316cd7661918b55c1b
Nick Reid <nickreid@nickreid.com>
2018-09-29 20:19:41 -0700

    Entered data, removed discomfort effect

--- client/src/app/medication-effects.service.ts
+++ client/src/app/medication-effects.service.ts
@@ -4,18 +4,14 @@ const days: Array<number> = [0, 2, 7, 14, 30, 90, 365, 1095];
 
 const effects: Array<any> = [
     {
-        key: 'cravings',
-        name: 'Effectivness of reducing cravings'
-    },
-    {
         key: 'mortality',
         name: 'Effectivness of reducing mortality'
     }, {
         key: 'relapse',
         name: 'Effectivness of reducing relapse'
     }, {
-        key: 'discomfort',
-        name: 'Discomfort'
+        key: 'cravings',
+        name: 'Effectivness of reducing cravings'
     }, {
         key: 'doseFrequency',
         name: 'Dose Frequency'
@@ -55,15 +51,11 @@ export class MedicationEffectsService {
     constructor() {
         this.effects = {};
         medications.forEach((medication) => {
-            const defaultTimes = {};
-            days.forEach((time) => {
-                const effectTypes = {};
-                effects.forEach((effect) => {
-                    effectTypes[effect.key] = '';
-                });
-                defaultTimes[time] = effectTypes;
+            const effectTypes = {};
+            effects.forEach((effect) => {
+                effectTypes[effect.key] = {};
             });
-            this.effects[medication.key] = defaultTimes;
+            this.effects[medication.key] = effectTypes;
         });
 
         this.addEffect('MMT', 0, 'cravings', 'high');
@@ -78,36 +70,88 @@ export class MedicationEffectsService {
         this.addEffect('BPNINJ', 2, 'cravings', 'moderate');
         this.addEffect('BPNIMP', 2, 'cravings', 'moderate');
         this.addEffect('NTXXR', 2, 'cravings', 'moderate');
-        this.addEffect('NONE', 2, 'cravings', 'high');
 
         this.addEffect('MMT', 7, 'cravings', 'none');
         this.addEffect('BPNSL', 7, 'cravings', 'mild');
         this.addEffect('BPNINJ', 7, 'cravings', 'mild');
         this.addEffect('BPNIMP', 7, 'cravings', 'mild');
-        this.addEffect('NTXXR', 7, 'cravings', 'moderate');
-        this.addEffect('NONE', 7, 'cravings', 'high');
 
-        this.addEffect('MMT', 14, 'cravings', 'none');
         this.addEffect('BPNSL', 14, 'cravings', 'none');
         this.addEffect('BPNINJ', 14, 'cravings', 'none');
         this.addEffect('BPNIMP', 14, 'cravings', 'none');
-        this.addEffect('NTXXR', 14, 'cravings', 'moderate');
         this.addEffect('NONE', 14, 'cravings', 'none');
 
-        this.addEffect('MMT', 30, 'cravings', 'none');
-        this.addEffect('BPNSL', 30, 'cravings', 'none');
-        this.addEffect('BPNINJ', 30, 'cravings', 'none');
-        this.addEffect('BPNIMP', 30, 'cravings', 'none');
         this.addEffect('NTXXR', 30, 'cravings', 'none');
-        this.addEffect('NONE', 30, 'cravings', 'none');
+
+        this.addEffect('MMT', 0, 'doseLocation', 'clinic');
+        this.addEffect('MMT', 0, 'doseFrequency', 'daily');
+        this.addEffect('MMT', 90, 'doseLocation', 'home');
+
+        this.addEffect('BPNSL', 0, 'doseLocation', 'home');
+        this.addEffect('BPNSL', 0, 'doseFrequency', 'daily');
+
+        this.addEffect('BPNINJ', 0, 'doseLocation', 'home');
+        this.addEffect('BPNINJ', 0, 'doseFrequency', 'daily');
+        this.addEffect('BPNINJ', 7, 'doseFrequency', 'monthly');
+        this.addEffect('BPNINJ', 7, 'doseLocation', 'clinic');
+
+        this.addEffect('BPNIMP', 0, 'doseLocation', 'home');
+        this.addEffect('BPNIMP', 0, 'doseFrequency', 'daily');
+        this.addEffect('BPNIMP', 7, 'doseFrequency', 'biannually');
+        this.addEffect('BPNIMP', 7, 'doseLocation', 'clinic');
+
+        this.addEffect('NTXXR', 0, 'doseLocation', 'home');
+        this.addEffect('NTXXR', 0, 'doseFrequency', 'daily');
+        this.addEffect('NTXXR', 14, 'doseLocation', 'clinic');
+        this.addEffect('NTXXR', 14, 'doseFrequency', 'monthly');
+
+        this.addEffect('MMT', 0, 'mortality', 50);
+        this.addEffect('MMT', 365, 'mortality', 35);
+
+        this.addEffect('BPNSL', 0, 'mortality', 70);
+        this.addEffect('BPNINJ', 0, 'mortality', 70);
+        this.addEffect('BPNIMP', 0, 'mortality', 70);
+        this.addEffect('NTXXR', 0, 'mortality', 70);
+        this.addEffect('NONE', 0, 'mortality', 100);
+
+        this.addEffect('MMT', 0, 'relapse', 50);
+        this.addEffect('BPNSL', 0, 'relapse', 50);
+        this.addEffect('BPNINJ', 0, 'relapse', 50);
+        this.addEffect('BPNIMP', 0, 'relapse', 50);
+        this.addEffect('NTXXR', 0, 'relapse', 50);
+        this.addEffect('NONE', 0, 'relapse', 100);
+
     }
 
     addEffect(medication: string, time: number, effectType: string, value: any) {
-        this.effects[medication][time][effectType] = value;
+        this.effects[medication][effectType][time] = value;
     }
 
     getEffect(medication: string, time: number, effectType: string) {
-        return this.effects[medication][time][effectType];
+        const medicationEffects = this.effects[medication][effectType];
+        if (medicationEffects[time]) {
+            return medicationEffects[time];
+        } else {
+            const times: Array<Number> = [];
+            Object.keys(medicationEffects).forEach((value: string) => {
+                times.push(Number(value));
+            });
+            times.sort((a: Number, b: Number) => {
+                if (a < b) {
+                    return -1;
+                } else if (b > a) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
+            let t: Number;
+            while (times[0] < time && times.length) {
+                t = times[0];
+                times.shift();
+            }
+            return medicationEffects[String(t)];
+        }
     }
 
     getEffects() {
