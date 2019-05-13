338e43161abbb5a1400268106171bdd79eb1c582
Nick Reid <nickreid@nickreid.com>
2019-05-12 17:58:55 -0700

    Add medication and effects models

--- server/medications/admin.py
+++ server/medications/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Effect
+from .models import Medication
+
+@admin.register(Effect)
+class EffectAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Medication)
+class MedicationAdmin(admin.ModelAdmin):
+    pass
--- server/medications/models.py
+++ server/medications/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class BaseObject(models.Model):
+    name = models.CharField(max_length=150)
+    slug = models.CharField(max_length=250)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+class Medication(BaseObject):
+    pass
+
+class Effect(BaseObject):
+    pass
+
+class MedicationEffect(models.Model):
+    medication = models.ForeignKey(Medication, on_delete=models.CASCADE)
+    effect = models.ForeignKey(Effect, on_delete=models.CASCADE)
+
+    short_description = models.CharField(max_length=250, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return '%s: %s' % (self.medication, self.effect)
