430586e19b0a5505213a63f938cae80ac5308bce
Nick Reid <nickreid@nickreid.com>
2019-05-13 09:22:04 -0700

    Add published boolean

--- server/chapters/models.py
+++ server/chapters/models.py
@@ -12,12 +12,16 @@ class Chapter(OrdableContent):
     title = models.CharField(max_length=250)
     slug = models.CharField(max_length=250, unique=True)
 
+    published = models.BooleanField(default=True)
+
     def __str__(self):
         return self.title
 
 class Page(OrdableContent):
     title = models.CharField(max_length=250)
     slug = models.CharField(max_length=250, unique=True)
+
+    published = models.BooleanField(default=True)
 
     chapter = models.ForeignKey(
         Chapter,
