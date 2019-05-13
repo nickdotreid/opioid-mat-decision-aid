11d2f558d4ca728be732a445148daecbfc18884f
Nick Reid <nickreid@nickreid.com>
2019-05-13 09:20:23 -0700

    Add simple chapters and pages

--- server/chapters/admin.py
+++ server/chapters/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from admin_ordering.admin import OrderableAdmin
+
+from .models import Chapter
+from .models import Page
+
+class PageInline(OrderableAdmin, admin.TabularInline):
+    model = Page
+    ordering = ['order']
+    ordering_field = 'order'
+
+@admin.register(Chapter)
+class ChapterAdmin(OrderableAdmin, admin.ModelAdmin):
+    ordering = ['order']
+    ordering_field = 'order'
+
+    list_display = ['title', 'order']
+    list_editable = ['order']
+
+    inlines = [
+        PageInline
+    ]
--- server/chapters/models.py
+++ server/chapters/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class OrdableContent(models.Model):
+    order = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+        ordering = ['order']
+
+
+class Chapter(OrdableContent):
+    title = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Page(OrdableContent):
+    title = models.CharField(max_length=250)
+    slug = models.CharField(max_length=250, unique=True)
+
+    chapter = models.ForeignKey(
+        Chapter,
+        on_delete=models.CASCADE,
+        related_name="pages"
+    )
