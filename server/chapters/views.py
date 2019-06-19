259f3c24e833ef60e309458dbb5114d0a551c5d9
Nick Reid <nickreid@nickreid.com>
2019-06-18 20:20:41 -0700

    Add quiz to page

--- server/chapters/views.py
+++ server/chapters/views.py
@@ -4,16 +4,18 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from charts.views import ChartSerializer
+from quizes.views import QuizSerializer
 
 from .models import Chapter
 from .models import Page
 
 class PageSerializer(serializers.ModelSerializer):
     chart = ChartSerializer(read_only=True)
+    quiz = QuizSerializer(read_only=True)
 
     class Meta:
         model = Page
-        fields = ('slug', 'title', 'content', 'chart')
+        fields = ('slug', 'title', 'content', 'chart', 'quiz')
 
 class ChapterSerializer(serializers.ModelSerializer):
     pages = PageSerializer(many=True, read_only=True)
--- server/quizes/views.py
+++ server/quizes/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Quiz
+
+class QuizSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Quiz
+        fields = ('slug', 'title')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['questions'] = self.serialize_questions(instance)
+        return representation
+
+    def serialize_questions(self, instance):
+        questions = []
+        for question in instance.questions.all():
+            questions.append({
+                'text': question.text,
+                'options': [
+                    {
+                        'name': 'Yes',
+                        'slug': 'yes',
+                        'value': 1
+                    }, {
+                        'name': 'No',
+                        'slug': 'no',
+                        'value': 0
+                    }
+                ]
+            })
+        return questions
