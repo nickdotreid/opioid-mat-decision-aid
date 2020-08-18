79e0c34e9a1d8de6a83fd46c8dd67cece8b361b1
Nick Reid <nickreid@nickreid.com>
2020-08-17 19:25:27 -0700

    Change layout to use css grid

--- client/src/app/page.component.ts
+++ client/src/app/page.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     templateUrl: './page.component.html',
+    // tslint:disable-next-line:use-host-property-decorator
+    host: {
+        'class': 'full-width'
+    }
 })
 export class PageComponent {
     constructor() {}
